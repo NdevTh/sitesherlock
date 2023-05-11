8c2debabc30344ef6002952d1ec5ad1efb81f2ff
HUYNH CongThuc <congthuc.huynh@al-enterprise.com>
2023-05-11 15:23:29 +0200

    Site en ligne

---
+++ src/echart/echartMrr.js
@@ -0,0 +1,24 @@
+
+
+var chartDom = document.getElementById("mrr");
+var myChart = echarts.init(chartDom);
+var option;
+
+option = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: [150, 230, 224, 218, 135, 147, 260],
+      type: 'line'
+    }
+  ]
+};
+
+console.log("echart> settting option")
+if(option) myChart.setOption(option);
